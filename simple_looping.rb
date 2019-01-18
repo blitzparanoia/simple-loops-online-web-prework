f2aa2ce2b93bb830ed428570a7ab16ff13f12adb
Irene Saez <bluemilk44@gmail.com>
2019-01-18 22:57:55 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,6 +12,7 @@ def loop_iterator(number_of_times)
   loop do
     counter = counter + 1
     phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     if counter >= 7
       break
   end
