f4ef96d7816574f45d018a2d00dc4be311c696b5
Irene Saez <bluemilk44@gmail.com>
2019-01-18 23:06:14 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,21 +1,3 @@
-Skip to content
- 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- @blitzparanoia Sign out
-30
-0 147 learn-co-students/simple-loops-online-web-prework
- Code  Issues 0  Pull requests 145  Projects 0  Insights
-simple-loops-online-web-prework/simple_looping.rb
-1adf243  on Jun 29, 2015
-@SophieDeBenedetto SophieDeBenedetto updated readme, updated methods to incl hints in comments
-@irmiller22 @SophieDeBenedetto
-     
-43 lines (30 sloc)  1.48 KB
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
@@ -57,17 +39,3 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
-
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
